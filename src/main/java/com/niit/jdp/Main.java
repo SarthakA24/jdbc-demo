a57e8533c07aac9c4d724747fe8a3be756cbd780
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-16 11:53:18 +0530

    created an object for the DatabaseService and connected to the database

--- src/main/java/com/niit/jdp/Main.java
+++ src/main/java/com/niit/jdp/Main.java
@@ -1,7 +1,20 @@
 package com.niit.jdp;
 
+import com.niit.jdp.service.database.DatabaseService;
+
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DatabaseService databaseService = new DatabaseService();
+        try {
+            boolean isDatabaseConnected = databaseService.connect();
+            System.out.println("isDatabaseConnected = " + isDatabaseConnected);
+        } catch (ClassNotFoundException | SQLException exception) {
+            System.err.println("Error wile connecting to the database!");
+            exception.printStackTrace();
+        } finally {
+            databaseService.printConnectionStatus();
+        }
     }
 }
