d2f30e10e0dd88885d8b4a0551b2c59501231df1
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-16 11:20:17 +0530

    added the method signature for the connect() method

--- src/main/java/com/niit/jdp/service/database/DatabaseService.java
+++ src/main/java/com/niit/jdp/service/database/DatabaseService.java
@@ -1,4 +1,8 @@
 package com.niit.jdp.service.database;
 
 public class DatabaseService {
+    // A method that can connect to the database
+    public boolean connect() {
+        return false;
+    }
 }
