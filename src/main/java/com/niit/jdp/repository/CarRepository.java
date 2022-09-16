72b6d86ea2cd49aac6c06eda8d94be686b343704
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-16 13:35:39 +0530

    created a method to write the Car object in the database using INSERT SQL Query

--- src/main/java/com/niit/jdp/repository/CarRepository.java
+++ src/main/java/com/niit/jdp/repository/CarRepository.java
@@ -1,4 +1,13 @@
 package com.niit.jdp.repository;
 
+import com.niit.jdp.model.Car;
+
+import java.sql.Connection;
+
 public class CarRepository {
+
+    // Create a method to write the Car object in the database using INSERT SQL Query
+    public boolean createCarInDatabase(Car car, Connection connection) {
+        return false;
+    }
 }
