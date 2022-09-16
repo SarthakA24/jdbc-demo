5408cb0d7b25e19ba2ad5f65985a05349faa465a
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-16 13:36:17 +0530

    wrote the SQL query for inserting a car row into the car table

--- src/main/java/com/niit/jdp/repository/CarRepository.java
+++ src/main/java/com/niit/jdp/repository/CarRepository.java
@@ -8,6 +8,8 @@ public class CarRepository {
 
     // Create a method to write the Car object in the database using INSERT SQL Query
     public boolean createCarInDatabase(Car car, Connection connection) {
+        // 1. write the SQL query for inserting a car row into the car table
+        String insertCarQuery = "INSERT INTO `carsales`.`car` (`name`, `model`, `cost_in_rupees`) VALUES" + " (?, ?, ?);";
         return false;
     }
 }
