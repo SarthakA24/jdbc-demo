ad7ae771a57bf966306282aa80c792457222a430
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-16 13:43:22 +0530

    prompted the user to input the car details to be added in the database

--- src/main/java/com/niit/jdp/Main.java
+++ src/main/java/com/niit/jdp/Main.java
@@ -3,6 +3,7 @@ package com.niit.jdp;
 import com.niit.jdp.service.database.DatabaseService;
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,6 +11,16 @@ public class Main {
         try {
             boolean isDatabaseConnected = databaseService.connect();
             System.out.println("isDatabaseConnected = " + isDatabaseConnected);
+            // prompt the user to input the car details to be added in the database
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Hello, please enter the details of your car");
+            System.out.println("Please enter the name of the car");
+            String name = scanner.nextLine();
+            System.out.println("Please enter the model of the car");
+            String model = scanner.nextLine();
+            System.out.println("Please enter the cost of the car");
+            double cost = scanner.nextDouble();
+
         } catch (ClassNotFoundException | SQLException exception) {
             System.err.println("Error wile connecting to the database!");
             exception.printStackTrace();
