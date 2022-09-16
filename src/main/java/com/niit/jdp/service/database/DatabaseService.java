b121a851c97aed8861fe9cb29d2fdf3538fdb99b
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-16 11:24:27 +0530

    added the required details for the database connection

--- src/main/java/com/niit/jdp/service/database/DatabaseService.java
+++ src/main/java/com/niit/jdp/service/database/DatabaseService.java
@@ -1,6 +1,17 @@
 package com.niit.jdp.service.database;
 
+import java.sql.Connection;
+
 public class DatabaseService {
+
+    // URL of the database
+    private static final String URL = "jdbc:mysql://localhost:3306/CarSales";
+    // Credentials for the user trying to connect to the database
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Sarthak@24";
+    // A private field to store the reference of the connection object
+    private Connection databaseConnection;
+
     // A method that can connect to the database
     public boolean connect() {
         return false;
