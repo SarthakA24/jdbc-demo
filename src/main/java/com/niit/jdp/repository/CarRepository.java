1b8119c5720ee9154999225fe5cfa7a3e2a237b0
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-16 13:38:44 +0530

    used the connection object to execute the query

--- src/main/java/com/niit/jdp/repository/CarRepository.java
+++ src/main/java/com/niit/jdp/repository/CarRepository.java
@@ -3,13 +3,23 @@ package com.niit.jdp.repository;
 import com.niit.jdp.model.Car;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class CarRepository {
 
     // Create a method to write the Car object in the database using INSERT SQL Query
-    public boolean createCarInDatabase(Car car, Connection connection) {
+    public boolean createCarInDatabase(Car car, Connection connection) throws SQLException {
         // 1. write the SQL query for inserting a car row into the car table
         String insertCarQuery = "INSERT INTO `carsales`.`car` (`name`, `model`, `cost_in_rupees`) VALUES" + " (?, ?, ?);";
+        // 2. use the connection object to execute the query
+        int numberOfRowsAffected;
+        try (PreparedStatement preparedStatement = connection.prepareStatement(insertCarQuery)) {
+            preparedStatement.setString(1, car.getName());
+            preparedStatement.setString(2, car.getModel());
+            preparedStatement.setDouble(3, car.getCostInRupees());
+            numberOfRowsAffected = preparedStatement.executeUpdate();
+        }
         return false;
     }
 }
